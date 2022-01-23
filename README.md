b65ea94b9dfcf5464c6435adcf0cf0e67d091ee3
Elliot Bobrow <elliotgreybobrow@gmail.com>
2022-01-23 15:57:37 -0800

    generate basic graph and table of values

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chi Square
+
+Running `cargo r -- -d 5 -s 1000` produced
+
+![Program output](/img/chi-demo.png)
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def main(data):
     df = pd.DataFrame(data)
-    print(df)
+    df = df[sorted(df.columns)]
+    # print(df)
+    for label, content in df.iteritems():
+        print(str(label) + ": " + str(np.percentile(content, 95)))
+    df.plot.hist(subplots=True, bins=20)
+    plt.show()
