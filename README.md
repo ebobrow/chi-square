7bc07da649d8412f0f2b71c861e2a12d514bfa37
Elliot Bobrow <elliotgreybobrow@gmail.com>
2022-01-23 17:06:10 -0800

    more quantiles

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Running `cargo r -- -d 5 -s 1000` produced
 
-![Program output](/img/chi-demo.png)
+![Program output](/img/chi-demo-new.png)
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from functools import partial
+
+q_95 = partial(pd.Series.quantile, q=0.95)
+q_95.__name__ = "95%"
+
+q_99 = partial(pd.Series.quantile, q=0.99)
+q_99.__name__ = "99%"
 
 def main(data):
     df = pd.DataFrame(data)
     df = df[sorted(df.columns)]
-    # print(df)
-    for label, content in df.iteritems():
-        print(str(label) + ": " + str(np.percentile(content, 95)))
+    print(df.agg([q_95, q_99]))
     df.plot.hist(subplots=True, bins=20)
     plt.show()
