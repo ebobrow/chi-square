c6fef55cfd4c6cb39aaf84e964c07e7e1c0cd601
Elliot Bobrow <elliotgreybobrow@gmail.com>
2022-02-08 17:42:48 -0800

    experimentation

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 # Chi Square
 
-Running `cargo r --release -- --df 5 --sets 10000 --reps 100000` produced
-
-![Program output](/img/10000x100000.png)
-
-Running `cargo r --release -- --df 10 --sets 10000 --reps 100000` produced
-
-![Program output](/img/10df.png)
+![Program output](/img/10000000x10000.png)
 
 More examples in /img folder
 
--- main.py
+++ main.py
@@ -12,6 +12,22 @@ q_99.__name__ = "99%"
 def main(data):
     df = pd.DataFrame(data)
     df = df[sorted(df.columns)]
-    print(df.agg([q_95, q_99]))
-    df.plot.hist(subplots=True, bins=20)
-    plt.show()
+    expected = pd.DataFrame({
+                                1:[3.841, 6.635],
+                                2:[5.991, 9.210],
+                                3:[7.815, 11.34],
+                                4:[9.488, 13.28],
+                                5:[11.07, 15.09]
+                            },
+                            index=['95%', '99%'])
+
+    # Show 95th and 99th quantile
+    table = df.agg([q_95, q_99])
+    print(table)
+
+    # Show difference from expected
+    # print(table.subtract(expected))
+
+    # Display graphs
+    # df.plot.hist(subplots=True, bins=20)
+    # plt.show()
